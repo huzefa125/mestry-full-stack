ff0f3a8c0c8a1c85ea9689f9d29242f9ab2ddcd6
Huzefa <Huzafahuzafa63@gmail.com>
2025-06-22 18:37:51 +0530

    first commit

---
+++ Props-state/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar({data}) {
+  return (
+    <div className='w-full px-4 py-3 flex justify-between items-center'>
+      <h3>Orange</h3>
+      <div className='flex p-2 px-4 bg-orange-600 text-white rounded-md text-sm gap-3'>
+        <h3>Favourties</h3>
+        <h4>{data.filter(item =>item.added).length}</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
